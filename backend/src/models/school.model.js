27eab7ed54b1ab0b451499ac334ae07e1afde086
Pablo-Genial <sporthold@yahoo.com>
2021-09-06 13:31:17 +0200

    2. 3. solved

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,21 @@
-/**
- * Exercise: make School mongoose model
- */
+const mongoose = require('mongoose')
+
+const SchoolSchema = mongoose.Schema(
+  {
+    name: String,
+    city: String,
+    street: String,
+    zipcode: Number,
+    classroom: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Classroom',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model('School', SchoolSchema)
